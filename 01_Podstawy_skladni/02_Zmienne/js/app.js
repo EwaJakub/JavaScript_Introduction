a3c41583bc333e3c8b91cda97f3fd98f315c7b96
EwaJakub <ewa.jak@onet.pl>
2023-11-02 22:07:21 +0100

    02_Zmienne

---
+++ 01_Podstawy_skladni/02_Zmienne/js/app.js
@@ -0,0 +1,11 @@
+const a = 2; //zdefiniowanie zmiennej
+console.log(a); //wypisanie wartości
+
+const b = "Ala ma kota"; //zdefiniowanie zmiennej
+console.log(b); //wypisanie wartości
+
+let user = "Ewa";
+console.log(user); //wypisanie wartości
+
+var zmienna = "34";
+console.log(zmienna); //wypisanie wartości
